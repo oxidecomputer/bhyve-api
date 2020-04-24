3140ad86b09f378db045ae6ed1bf8949a9c14dfc
Allison Randal <allison@oxide.computer>
2020-04-24 10:51:11 -0400

    Intermediate stage of debugging VMX exit

--- examples/asm.rs
+++ examples/asm.rs
@@ -15,15 +15,27 @@ use std::ptr::null_mut;
 
 const BSP: i32 = 0;
 
+const RTC_LMEM_LSB: i32 = 0x34;
+const RTC_LMEM_MSB: i32 = 0x35;
+
+const MB: usize = (1024 * 1024);
+
+const m_64KB: usize = (64*1024);
+const m_16MB: usize = (16*1024*1024);
+
+
+
 fn main() {
     let vm_name = "helloworld";
-    let mem_size: usize = 0x4000;
+    let mem_size: usize = 20 * MB;
+    let guest_addr: usize = 0xfff0;
     let asm_code: &[u8] = &[
         0xba, 0xf8, 0x03, /* mov $0x3f8, %dx */
         0x00, 0xd8,       /* add %bl, %al */
         0x04, b'0',       /* add $'0', %al */
         0xee,             /* out %al, %dx */
-        0xec,             /* in %dx, %al */
+        0xb0, 0x0a,       /* mov $'\n', %al */
+        0xee,             /* out %al, %dx */
         0xf4,             /* hlt */
     ];
 
@@ -47,26 +59,37 @@ fn main() {
     let vm = VirtualMachine::new(vm_name).expect("failed to open filehandle to VM device");
     println!("Opened a filehandle to /dev/vmm/{}", vm.name);
 
+    vm.reinit().expect("failed to re-initialize VM");
     vm.set_topology(1, 1, 1).expect("failed to set CPU topology");
     vm.set_x2apic_state(BSP, false).expect("failed to disable x2APIC");
 
     vm.setup_lowmem(host_addr as u64, mem_size).expect("failed to set guest memory");
 
-    // Write the x86 assembly code in the guest memory.
-    unsafe {
-        let mut slice = slice::from_raw_parts_mut(host_addr, mem_size);
-        slice.write(&asm_code).unwrap();
-    }
+    let lomem = (mem_size - m_16MB) / m_64KB;
+    vm.rtc_write(RTC_LMEM_LSB, lomem as u8).expect("failed to set RTC memory size");
+    vm.rtc_write(RTC_LMEM_MSB, (lomem >> 8) as u8).expect("failed to set RTC memory size");
 
-    let rip = vm.get_register(BSP, vm_reg_name::VM_REG_GUEST_RIP).unwrap();
-    println!("RIP reg is {}", rip);
+    // Write the x86 assembly code in the guest memory.
+    let offset: u64 = host_addr as u64 + guest_addr as u64;
+    let slice_size = mem_size - guest_addr;
+    unsafe {
+        let mut slice = slice::from_raw_parts_mut(offset as *mut u8, slice_size);
+        slice.write(&asm_code).unwrap();
+//        let slice = slice::from_raw_parts_mut(host_addr, mem_size);
+//        let (_, mut offset) = slice.split_at_mut(guest_addr / 8);
+//        offset.write(&asm_code).unwrap();
+    }
 
     // Setup registers
     vm.set_register(BSP, vm_reg_name::VM_REG_GUEST_CS, 0).expect("failed to set CS register");
 
+    vm.set_register(BSP, vm_reg_name::VM_REG_GUEST_RIP, guest_addr as u64).expect("failed to set RIP register");
     vm.set_register(BSP, vm_reg_name::VM_REG_GUEST_RAX, 2).expect("failed to set RAX register");
     vm.set_register(BSP, vm_reg_name::VM_REG_GUEST_RBX, 3).expect("failed to set RBX register");
-    vm.set_register(BSP, vm_reg_name::VM_REG_GUEST_RFLAGS, 3).expect("failed to set RFLAGS register");
+    vm.set_register(BSP, vm_reg_name::VM_REG_GUEST_RFLAGS, 0x2).expect("failed to set RFLAGS register");
+
+    let rip = vm.get_register(BSP, vm_reg_name::VM_REG_GUEST_RIP).unwrap();
+    println!("RIP reg is {}", rip);
 
     match vm.activate_vcpu(BSP) {
         Ok(_) => println!("Activated CPU 0 for VM at /dev/vmm/{}", vm_name),
@@ -78,8 +101,8 @@ fn main() {
             VmExit::InOut => {
                 println!("exit for InOut");
             }
-            VmExit::Vmx => {
-                println!("exit for Vmx");
+            VmExit::Vmx(s, r, q, t, e) => {
+                println!("exit for Vmx, source={}, reason={}, qualification={}, inst type={}, inst error={}", s, r, q, t, e);
                 break;
             }
             VmExit::Halt => {
